70eb80ad8a080c289ae5d13c4e58c847ec7f0eeb
sidp <sidp@umich.edu>
2019-09-06 15:51:39 -0400

    Pushing some python code

---
+++ helloworld.py
@@ -0,0 +1,9 @@
+import random
+print("Sid Pagariya")
+num1 = random.randint(0, 100)
+print(num1)
+num2 = random.randint(0, 100)
+print(num2)
+sum = num1+num2
+print("Sum =", sum)
+print("Average =", sum/2) 
